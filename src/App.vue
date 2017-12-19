0bfee1e781c7e21dd9159cc5a91676f4bd6025fc
CanXue <canxue_1314@163.com>
2017-12-19 17:18:14 +0800

    create a yixi-music project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+    import MHeader from '@/components/m-header/m-header'
+
+    export default{
+      components: {
+        MHeader
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "./common/styles/variable";
+
+#app{
+  color: $color-theme;
+}
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="m-header">
+        <div class="icon"></div>
+        <h1 class="text">Yixi Music</h1>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style lang="scss" scoped>
+@import "../../common/styles/variable";
+@import "../../common/styles/mixin";
+
+.m-header{
+    position: relative;
+    height: 40px;
+    text-align: center;
+    color: $color-theme;
+    font-size: 0;
+    .icon{
+        display: inline-block;
+        vertical-align: top;
+        margin-top: 6px;
+        width: 30px;
+        height: 32px;
+        margin-right: 9px;
+        @include bg-image('logo');
+        background-size: 30px 32px;
+    }
+    .text{
+        display: inline-block;
+        vertical-align: top;
+        line-height: 44px;
+        font-size: $font-size-large
+    }
+}
+</style>
